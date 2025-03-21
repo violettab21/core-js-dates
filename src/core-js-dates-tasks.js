5b42aa8cf9121d58ae204ae3fdfe8004d15596a0
violettab <violettabatsura@gmail.com>
2025-03-21 22:31:29 +0300

    feat: solve getCountDaysOnPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -134,8 +134,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const diff = new Date(dateEnd) - new Date(dateStart);
+  const days = diff / 1000 / 3600 / 24;
+  return days + 1;
 }
 
 /**
